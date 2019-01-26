32fc4624a30013a4e6cea777fd49993322ec8aeb
wmcrockett <wmcrockett@gmail.com>
2019-01-26 17:47:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/talkin twinks$/;
+      botRegex = /^\/talkin taters/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -32,7 +32,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" : "R U TALKIN TWINKS 2 ME???"
+    "text" : "shayaan a sweet potato snack"
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
